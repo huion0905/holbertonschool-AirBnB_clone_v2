efe34ad0c21fab55a362280a1d1424322d033496
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-04-12 22:04:42 -0500

    update

--- models/user.py
+++ models/user.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+<<<<<<< HEAD
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+=======
+from sqlalchemy import Column, String # type: ignore
+from sqlalchemy.orm import relationship # type: ignore
+>>>>>>> 9d8f76340052d4583dad0a15bfc3d6c5a75db260
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
